b04b7f83af2cddd3d011be9e50d2fdf8bcfb4041
Santiago <santiagowhite99@hotmail.com>
2020-04-13 18:44:48 -0500

    Refactor de codigo. Pasa a ser FEAN stack. Queda app.js con express y firestore funcionando

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
-const React = require('react');
-
 const express = require('express');
-const app = express();
-const port = 3000;
+var app = express();
 
-//document.createElement('as', "        ");
+//Firestore initialization
+const admin = require('firebase-admin');
+let serviceAccount = require('./firebase-key.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+let db = admin.firestore();
 
-app.use(express.static(__dirname + "/public"));
-//app.set("view engine","html");
 
-app.get('/', (req, res) => res.sendfile("./public/index.html"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+
+
+
+//ROOT
+app.get('/', async (req,res)=> {
+    //ejemplo de uso de firestore
+    let docRef = db.collection('users').doc('alovelace');
+    let setAda =  await docRef.set({
+        first: 'Ada',
+        last: 'Lovelace',
+        born: 1815
+    });
+    console.log(setAda);
+
+    res.send('Home page working');
+
+
+});
+
+
+
+
+
+
+
+
+
+
+//Get the server running
+const port = process.env.PORT || 5000;
+const host = process.env.HOST || 'localhost';
+app.listen(port, host);
+console.log(`Your server is running on ${host}:${port}`);
--- modules/Blog.js
+++
@@ -1,2 +0,0 @@
-const route = 'index'; 	// Route: 'recipe' routes to /recipe
-const modelId = 'Blog';  	// Same name as file, no extension: Recipe'
